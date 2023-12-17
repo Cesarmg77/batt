397eb84a4bdd6d3b8cb2a213492db8fe4b8e2776
Cesarmg77 <137302734+Cesarmg77@users.noreply.github.com>
2023-12-17 16:47:23 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,13 +4,28 @@ function updateBatteryStatus() {
   navigator.getBattery().then(battery => {
     const batteryStatusElement = document.getElementById('batteryStatus');
     const batteryPercent = Math.round(battery.level * 100);
-    
+
     // Actualiza el texto
     batteryStatusElement.textContent = `Nivel de Batería: ${batteryPercent}%`;
 
-    // Actualiza el título de la página
-    document.title = `Batería: ${batteryPercent}%`;
+    // Actualiza el ícono dinámico
+    updateDynamicIcon(batteryPercent);
   });
+}
+
+function updateDynamicIcon(batteryPercent) {
+  // Cambia el ícono dinámico según el nivel de batería
+  const iconPath = `/path/to/icon${batteryPercent}.png`;
+
+  // Actualiza el ícono en el manifest.json
+  const linkElement = document.querySelector('link[rel="manifest"]');
+  linkElement.href = `/manifest.json`;
+
+  // Actualiza el ícono en el tag de la aplicación
+  document.querySelector('link[rel="icon"]').href = iconPath;
+
+  // Actualiza el título de la página
+  document.title = `Batería: ${batteryPercent}%`;
 }
 
 // Llama a la función al cargar la página y cada vez que cambia el nivel de batería
